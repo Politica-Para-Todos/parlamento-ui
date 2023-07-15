cff1f99b83fef522b4e659510adb231e76da85a3
jopijuco <jpjc@protonmail.com>
2023-07-15 15:53:50 +0100

    First fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from 'next/image'
+import Test from './test'
+import { parse } from 'path';
 
-export default function Home() {
+export default async function Home() {
+  const fetching = async () => {
+    const response = await fetch('https://portuguese-politics.herokuapp.com/parliament/initiatives?legislature=XV&event_phase=Vota%C3%A7%C3%A3o%20na%20generalidade&limit=20&offset=0')
+    if (response.status !== 200) {
+      throw new Error('API request has failed');
+    }
+    const parsed = await response.json()
+    console.log(parsed);
+    return parsed['initiativas'] as Initiatives[];
+  }
+
+  const initiatives: Initiatives[] = await fetching();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -108,6 +122,45 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <div>
+        {initiatives.map(item => (
+          <li key={item.iniciativa_id}>{item.iniciativa_titulo}</li>
+        ))}
+      </div>
     </main>
   )
+}
+
+// interface Initiatives {
+//   id: string,
+//   fase: string,
+//   title: string,
+//   url: string,
+//   urlResource: string,
+//   author: string,
+//   deputies: string,
+//   date: string,
+//   type: string,
+//   outcome: string
+// }
+
+interface Initiatives {
+  iniciativa_id: string,
+  iniciativa_fase: string,
+  iniciativa_titulo: string,
+  iniciativa_url: string,
+  iniciativa_url_res: string,
+  iniciativa_autor: string,
+  iniciativa_autor_deputados_nomes: string,
+  iniciativa_data: string,
+  iniciativa_tipo: string,
+  iniciativa_votacao_res: string,
+  iniciativa_votacao_il: string,
+  iniciativa_votacao_psd: string,
+  iniciativa_votacao_be: string,
+  iniciativa_votacao_pan: string,
+  iniciativa_votacao_l: string,
+  iniciativa_votacao_pcp: string,
+  iniciativa_votacao_ps: string,
+  iniciativa_votacao_ch: string
 }
